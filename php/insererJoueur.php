158bb80370561b07d49f0c761cffd49141c65692
Damien Lanusse <damienl.work@gmail.com>
2023-02-03 11:55:22 +0100

    Ajout de commentaires et correction de petits détails

--- php/insererJoueur.php
+++ php/insererJoueur.php
@@ -1,25 +1,30 @@
 <?php
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
 
+    // On vérifie que toutes les variables nécessaires ont été définies
     if(isset($_POST['pseudo']) && isset($_POST['mail']) && isset($_POST['anneeNaiss']) && isset($_POST['mdp']) && isset($_POST['mdpVerif'])){
+
+        // On vérifie que le mot de passe a correctement été saisi deux fois
         if($_POST['mdp'] == $_POST['mdpVerif']){
             try{
+                // On définit les variables nécessaires au lien avec la BD
                 $bdd = "u562708442_Jdsel";
                 $host = "localhost";
                 $user= "u562708442_Grp4";
                 $pass = "u=5#5^xvcGEoKdq0>E";
+
+                // On définit les variables nécessaires à la commande
                 $nomtable = "Joueur";
                 $pseudonyme = $_POST['pseudo'];
                 $mail = $_POST['mail'];
                 $anneeNaiss = $_POST['anneeNaiss'];
                 $motDePasse = $_POST['mdp'];
 
-                $charsId = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-                $tabCharsId = str_split($charsId);
-                $idOK = false;
+                // On définit les variables nécessaires au calcul de l'identifiant
+                $charsId = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';    // Les caractères pouvant être utilisés
+                $tabCharsId = str_split($charsId);      // Un tableau des caractères pouvant être utilisés
+                $idOK = false;      // La variable nous permettant de rester dans la boucle while
         
+                // On fait le lien avec la BD
                 $link = mysqli_connect($host,$user,$pass,$bdd);
         
                 if (mysqli_connect_errno()){
@@ -27,20 +32,27 @@
                     throw new Exception();
                 }
         
+                // On définit l'identifiant
+                // Tant que nous n'avons pas un identifiant OK
                 while(!$idOK){
+                    // On définit l'identifiant
                     $identifiant = '';
+                    // On le remplit avec 16 caractères aléatoires parmi les caractères possibles
                     for($i = 0; $i < 16; $i++){
                         $index = array_rand($tabCharsId);
                         $identifiant .= $tabCharsId[$index];
                     }
-                    $query = "SELECT COUNT(*) FROM $nomtable WHERE identifiant = $identifiant";
+                    // On compte le nombre d'identifiants similaires
+                    $query = "SELECT COUNT(*) FROM $nomtable WHERE identifiant = '$identifiant'";
                     $result= mysqli_query($link, $query);
 
+                    // S'il n'y en a pas on sort de la boucle
                     if($result == 0){
                         $idOK = true;
                     }
                 }
 
+                // On créé la requête et on lance l'insertion d'un Joueur dans la base
                 $query = "INSERT INTO $nomtable VALUES('$identifiant', '$pseudonyme', $anneeNaiss, '$mail', '$motDePasse', NULL)";
                 $result = mysqli_query($link, $query);
         
@@ -49,11 +61,14 @@
                     throw new Exception();
                 }
         
+                // On ferme le lien avec la BD
                 mysqli_close($link);
                 
+                // Si l'insertion est réussie on continue
                 if($result == true){
                     header("Location: ../main.html");
                 }
+                // Sinon on marque qu'il y a eu un problème
                 else{
                     header('Location: ../creaCompte.html');
                     echo("<h4>Probleme lors de la creation de compte.</h4>");
--- php/testConnexion.php
+++ php/testConnexion.php
@@ -1,14 +1,19 @@
 <?php
+    // On vérifie si toutes les variables demandées ont été saisies
     if(isset($_POST['pseudo']) && isset($_POST['mdp'])){
         try{
+            // On définit les variables nécessaires au lien avec la BD
             $bdd = "u562708442_Jdsel";
             $host = "localhost";
             $user= "u562708442_Grp4";
             $pass = "u=5#5^xvcGEoKdq0>E";
+
+            // On définit les variables nécessaires à la commande
             $nomtable = "Joueur";
             $pseudonyme = $_POST['pseudo'];
             $motDePasse = $_POST['mdp'];
     
+            // On fait le lien avec la BD
             $link = mysqli_connect($host,$user,$pass,$bdd);
     
             if (mysqli_connect_errno()){
@@ -16,6 +21,7 @@
                 throw new Exception();
             }
     
+            // On créé et on exécute la commande
             $query = "SELECT COUNT(*) FROM $nomtable WHERE pseudonyme = '$pseudonyme' AND mdp = '$motDePasse'";
             $result= mysqli_query($link, $query);
     
@@ -24,12 +30,17 @@
                 throw new Exception();
             }
     
+            // On ferme le lien avec la BD
             mysqli_close($link);
             
+            // Si on obtient le résultat souhaité
             if($result == 1){
+                // On continue dans le site
                 header("Location: ../main.html");
             }
+            // Sinon
             else{
+                // On retourne au formulaire
                 header('Location: ../index.html');
                 echo("<h4>Le pseudonyme/mot de passe ne correspond pas</h4>");
             }
@@ -40,6 +51,7 @@
             echo "<p>Problème dans les commandes de la Base de Données</p>";
         }
     }
+    // Sinon on le signale
     else {
         header('Location: ../index.html');
         echo("<h4>Merci de remplir tous les champs</h4>");
