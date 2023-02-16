e142c3ad6e908fa75dfb094eb906a279c6ba778b
Damien Lanusse <damienl.work@gmail.com>
2023-02-16 16:52:16 +0100

    Modif 14.77

--- Jeux/Memory/client/src/nodeClasses/typeJoueurs/joueurHumain.mjs
+++ Jeux/Memory/client/src/nodeClasses/typeJoueurs/joueurHumain.mjs
@@ -7,7 +7,7 @@
 */
 
 // On importe la classe Joueur
-import { Joueur } from "../Joueur.mjs";
+require('../Joueur.mjs');
 
 // On crée la classe JoueurHumain qui étend la classe abstraite Joueur
 module.export = class JoueurHumain extends Joueur{
