0250657c43391b0e275b86096d3bdd3d952c0f17
Damien Lanusse <damienl.work@gmail.com>
2023-03-06 13:19:37 +0100

    Rooms version 1.55

--- Jeux/Memory/client/src/Fichiers-php/niveauMachine.php
+++ Jeux/Memory/client/src/Fichiers-php/niveauMachine.php
@@ -1,5 +1,9 @@
 <?php
 
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
     // Si on connait le niveau de la machine
     if(isset($_POST['lvlMachine'])){
         // On récupère le niveau de la machine
